5baa0112860b8d92290c8236be20f1f20c4ba9ad
Muktar16 <48786734+Muktar16@users.noreply.github.com>
2020-02-15 19:56:15 +0600

    Add files via upload

--- statistical analyzer.cpp
+++ statistical analyzer.cpp
@@ -5,6 +5,8 @@
 #include<conio.h>
 using namespace std;
 
+double Max,Min;
+
 void mainMenu();
 void unGroupMenu();
 void groupMenu();
@@ -13,7 +15,7 @@ void regAndCorMenu();
 void probabilityMenu();
 void formulaSheetsMenu();
 double sum(double *data,int dataNum);
-double* max_min_calculator(double *Data);
+void max_min_calculator(double *Data);
 double mean_calculator(double* Data,int dataNum);
 double gmean_calculator(double* Data,int dataNum);
 double hmean_calculator(double *Data,int dataNum);
@@ -23,7 +25,13 @@ double variance_calculator(double *Data,int dataNum);
 
 double variance_calculator(double *Data,int dataNum)
 {
-
+    double mean = mean_calculator(Data,dataNum);
+    double Sum=0;
+    for(int i=0;i<dataNum;i++)
+    {
+        Sum=Sum+(Data[i]-mean)*(Data[i]-mean);
+    }
+    return Sum/(dataNum-1);
 }
 
 double mode_calculator(double *Data,int dataNum)
@@ -89,18 +97,15 @@ double mean_calculator(double *Data, int dataNum)
     return Sum/dataNum;
 }
 
-double* max_min_calculator(double *Data,int dataNum)
+void max_min_calculator(double *Data,int dataNum)
 {
-    double MaxMin[2];
-    MaxMin[0]=MaxMin[1]=Data[0];
+    Max=Min=Data[0];
 
     for(int i=0;i<dataNum; i++)
     {
-        if(MaxMin[0]<Data[i]) MaxMin[0]=Data[i];
-        if(MaxMin[1]>Data[i]) MaxMin[1]=Data[i];
+        if(Max<Data[i]) Max=Data[i];
+        if(Min>Data[i]) Min=Data[i];
     }
-    return MaxMin;
-
 }
 
 double sum(double *Data,int dataNum)
@@ -113,66 +118,92 @@ double sum(double *Data,int dataNum)
     return sum;
 }
 
+void print_Growing_sequence(double *Data,int dataNum)
+{
+    for(int i=0;i<dataNum;i++)
+    {
+        for(int j=0;j<dataNum-i-1;j++)
+        {
+            if(Data[j]>Data[j+1])
+            {
+                double temp = Data[j];
+                Data[j]=Data[j+1];
+                Data[j+1]=temp;
+            }
+        }
+    }
+
+    for(int i=0;i<dataNum;i++)
+    {
+        printf("%.2f  ",Data[i]);
+    }
+}
+
 void unGroupMenu()
 {
+    system("cls");
     int dataNum;
     double Sum,Mean,GMean,HMean,Median,Mode,Range,Variance,SD,PCS;
     double *MaxMin;
 
-    printf("\n\t\t\t\tEnter the number of Data: ");
+    printf("\n\n\n\n\t\t\t\t\t\t\tSAMPLE SIZE: ");
     scanf("%d",&dataNum);
 
     double Data[dataNum];
 
-    printf("\n\t\t\t\tEnter data values with space: ");
+    printf("\n\t\t\t\t\t\t\tENTER %d VALUES WITH SPACE: ",dataNum);
     for(int i=0; i<dataNum; i++)
     {
         scanf("%lf",&Data[i]);
     }
 
     //print all data
-    printf("\n\n\t\t\t\tYour Data are:  ");
+//    Sum = sum(Data,dataNum);
+//    max_min_calculator(Data,dataNum);
+//    Mean = mean_calculator(Data, dataNum);
+//    GMean = gmean_calculator(Data, dataNum);
+//    HMean = hmean_calculator(Data,dataNum);
+//    Median = median_calculator(Data,dataNum);
+//    Mode = mode_calculator(Data,dataNum);
+//    Range = Max-Min;
+//    Variance = variance_calculator(Data,dataNum);
+
+    //print all values
+    system("cls");
+    printf("\n\n\t\t\t\t\t\t-> SAMPLE SEQUENCE = ");   //print data sequence
     for(int i=0;i<dataNum; i++)
     {
         printf("%.2f  ",Data[i]);
     }
+    printf("\n\t\t\t\t\t\t\t\t-> GROWING SEQUENCE = ");
+    print_Growing_sequence(Data,dataNum);
     printf("\n");
-
-    Sum = sum(Data,dataNum);
-    MaxMin = max_min_calculator(Data,dataNum);
-    Mean = mean_calculator(Data, dataNum);
-    GMean = gmean_calculator(Data, dataNum);
-    HMean = hmean_calculator(Data,dataNum);
-    Median = median_calculator(Data,dataNum);
-    Mode = mode_calculator(Data,dataNum);
-    Range =
-    Variance = variance_calculator(Data,dataNum);
-
-    //print all values
-    system("cls");
-    printf("\n\t\t\t\tSummation -> %.2f",Sum);
-//    printf("\n\t\t\t\tHighest value -> %.2f",MaxMin[0]);
-//    printf("\n\t\t\t\tLowest value -> %.2f",MaxMin[1]);
+    printf("\n\t\t\t\tSummation -> %.2f",sum(Data,dataNum));
+    max_min_calculator(Data,dataNum);
+    printf("\n\t\t\t\tHighest value -> %.2f",Max);
+    printf("\n\t\t\t\tLowest value -> %.2f",Min);
     printf("\n\t\t\t\tMean -> %.2f",Mean);
     printf("\n\t\t\t\tGeometric Mean -> %.2f",GMean);
     printf("\n\t\t\t\tHarmonic Mean -> %.2f",HMean);
     printf("\n\t\t\t\tMedian -> %.2f",Median);
     printf("\n\t\t\t\tMode -> %.2f",Mode);
     printf("\n\t\t\t\tRange -> %.2f",Range);
+    printf("\n\t\t\t\tVariance -> %.2f",Variance);
 
 }
 
 void main_menu()
 {
     int choice;
-    cout<<"\t\t\t\t1. UNGROUPED DATA\n";
-    cout<<"\t\t\t\t2. GROUPED DATA\n";
-    cout<<"\t\t\t\t3. UNGROUPED TO GROUPED DATA CONVERTER\n";
-    cout<<"\t\t\t\t4. REGRESSION & CO-RELATION\n";
-    cout<<"\t\t\t\t5. PROBABILITY DISTRIBUTION\n";
-    cout<<"\t\t\t\t6. FORMULA SHEETS\n";
-    cout<<"\t\t\t\t7. EXIT\n\n\n";
-    cout<<"\t\t\t\tEnter your Choice: ";
+    cout<<"\n\t\t\t\t\t\t\t\tMAIN MENU\n\t\t\t\t\t\t\t     ===============\n\n"
+        <<"\t\t\t\t\t\t\t1. UNGROUPED DATA\n"
+        <<"\t\t\t\t\t\t\t2. GROUPED DATA\n"
+        <<"\t\t\t\t\t\t\t3. UNGROUPED TO GROUPED DATA CONVERTER\n"
+        <<"\t\t\t\t\t\t\t4. REGRESSION & CO-RELATION\n"
+        <<"\t\t\t\t\t\t\t5. PROBABILITY DISTRIBUTION\n"
+        <<"\t\t\t\t\t\t\t6. FORMULA SHEETS\n"
+        <<"\t\t\t\t\t\t\t7. EXIT\n\n\n"
+        <<"\t\t\t\t\t\t\tEnter your Choice: ";
     cin>>choice;
 
     switch(choice)
@@ -184,14 +215,21 @@ void main_menu()
 //        case 5: probabilityMenu();
 //        case 6: formulaSheetsMenu();
         case 7: exit(0);
+        default:
+            {
+                printf("\nWrong Input.Try again.\n");
+                main_menu();
+            }
+
+
     }
 }
 
 int main()
 {
     cout<<"\n\n";
-    cout<<"\t\t\t\t================================================\n";
-    cout<<"\t\t\t\t\tWELCOME TO STATISTICAL CALCULATOR\n";
-    cout<<"\t\t\t\t================================================\n\n\n\n";
+    cout<<"\t\t\t\t\t\t================================================\n";
+    cout<<"\t\t\t\t\t\t\tWELCOME TO STATISTICAL CALCULATOR\n";
+    cout<<"\t\t\t\t\t\t================================================\n\n\n";
     main_menu();
 }
