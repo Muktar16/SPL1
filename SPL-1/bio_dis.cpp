167037c9b6397ba22c01ffd7edf4bc8ff382c2d4
Muktar16 <48786734+Muktar16@users.noreply.github.com>
2020-08-14 08:13:18 +0600

    Add files via upload

--- SPL-1/bio_dis.cpp
+++ SPL-1/bio_dis.cpp
@@ -3,10 +3,10 @@
 void bio_dis()
 {
     system("cls||clear");
-    double x,p;
-    int sign,n;
+    double p;
+    int sign,x,n;
     cout<<"\n\n\n\n\t\t\t\t\t\t\tEnter the value for X: ";
-    cin>>x;
+    scanf("%d",&x);
 
     cout<<"\n\n\n\n\t\t\t\t\t\t\t1. X = "<<x<<"\n"
     <<"\t\t\t\t\t\t\t2. X > "<<x<<"\n"
@@ -14,27 +14,52 @@ void bio_dis()
     <<"\t\t\t\t\t\t\t4. X >= "<<x<<"\n"
     <<"\t\t\t\t\t\t\t5. X <= "<<x<<"\n\n"
     <<"\t\t\t\t\t\t\tEnter a sign for X: ";
-    cin>>sign;
+    scanf("%d",&sign);
 
     cout<<"\n\t\t\t\t\t\t\tEnter the value for n: ";
-    cin>>n;
+    scanf("%d",&n);
     cout<<"\n\n\t\t\t\t\t\t\tEnter the value for P: ";
-    cin>>p;
+    scanf("%lf",&p);
 
     double mean=n*p;
-    int b;
+    double b;
     double variance=n*p*(1-p);
+
 
     switch(sign)
     {
         case 1:
-        {
-            b=(factorial(n)*pow(p,x)*pow(1-p,n-x))/(factorial(x)*factorial(n-x));
-            cout<<"\n\n\n\n\n\n\t\t\t\t\t\t\t\t";
-            cout<<"b("<<x<<";"<<n<<","<<p<<") = "<<b;
-            cout<<"Mean = "<<mean;
-            cout<<"Variance = "<<variance;
-        }
+            {
+                b=(factorial(n)*pow(p,x)*pow(1-p,n-x))/(factorial(x)*factorial(n-x));
+                cout<<"\n\n\n\n\n\n\t\t\t\t\t\t\t\t";
+                cout<<"b("<<x<<";"<<n<<","<<p<<") = "<<b;
+                cout<<"\n\n\t\t\t\t\t\t\t\tMean = "<<mean;
+                cout<<"\n\t\t\t\t\t\t\t\tVariance = "<<variance;
+                cout<<"\n\t\t\t\t\t\t\t\tStandard Deviation = "<<sqrt(variance);
+            }
+        case 2:
+            {
+                cout<<"\n\n\n\n";
+                for(int i=x+1;i<=n;i++)
+                {
+                    b=(factorial(n)*pow(p,i)*pow(1-p,n-i))/(factorial(i)*factorial(n-i));
+                    cout<<"\n\t\t\t\t\t\t\t\t b("<<i<<";"<<n<<","<<p<<") = "<<b<<"\n";
+                }
+                cout<<"\n\n\t\t\t\t\t\t\t\tMean = "<<mean;
+                cout<<"\n\t\t\t\t\t\t\t\tVariance = "<<variance;
+                cout<<"\n\t\t\t\t\t\t\t\tStandard Deviation = "<<sqrt(variance);
+            }
+        case 3:
+            {
+
+            }
+        case 4:
+            {
+
+            }
+
     }
+
+     _getch();
 
 }
--- SPL-1/factorial.cpp
+++ SPL-1/factorial.cpp
@@ -3,7 +3,7 @@
 
 int factorial(int n)
 {
-    int f;
+    int f=1;
     for(int i=1;i<=n;i++)
     {
         f*=i;
